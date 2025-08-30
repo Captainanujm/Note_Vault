7e7209f6e16804690ca094d6300c685a9787edcc
Anuj mishra <captainanuj2004@gmail.com>
2025-08-31 00:57:42 +0530

    improved ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+"use client";
 import Image from "next/image";
-
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 export default function Home() {
-  return (
-    <>
-   
-    </>
-  );
+  const router=useRouter();
+   useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      router.replace("/dashboard");
+    } else {
+      
+      router.replace("/signin");
+    }
+  }, [router]);
+
+  return null;
+
 }
