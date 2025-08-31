995b9d661bb16d9cd4608ee8f133704a736e8875
Anuj mishra <captainanuj2004@gmail.com>
2025-08-31 15:30:05 +0530

    email validation

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -15,6 +15,9 @@ const page = () => {
     const [name, setName] = useState("");
     const[getOTPclicked,setGetOTPClicked]=useState(false);
      const otpInputRef = useRef<HTMLInputElement>(null);
+     const isValidEmail = (email: string) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
      const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
          setEmail(e.target.value);
        }
@@ -30,6 +33,11 @@ const page = () => {
        const handleGetOTPClick=async()=>{
          try{
           setLoading(true);
+          if(!isValidEmail(email)){
+            toast.error("Please enter a valid email");
+            setLoading(false);
+            return;
+          }
            const res=await axios.post("https://note-vault-4.onrender.com/api/auth/get-otp",{email,name,dob});
        if(res.status===200){
         setLoading
@@ -67,7 +75,7 @@ const page = () => {
   }, [getOTPclicked]);
   return (
     <div className='flex'>
-    <div className='absolute lg:top-8 lg:left-8  flex justify-center items-center gap-1/2'>
+    <div className='absolute lg:top-8 m-4 lg:left-8  flex justify-center items-center gap-1/2'>
 <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M20.1424 0.843087L16.9853 0L14.3248 9.89565L11.9228 0.961791L8.76555 1.80488L11.3608 11.4573L4.8967 5.01518L2.58549 7.31854L9.67576 14.3848L0.845959 12.0269L0 15.1733L9.64767 17.7496C9.53721 17.2748 9.47877 16.7801 9.47877 16.2717C9.47877 12.6737 12.4055 9.75685 16.0159 9.75685C19.6262 9.75685 22.5529 12.6737 22.5529 16.2717C22.5529 16.7768 22.4952 17.2685 22.3861 17.7405L31.1541 20.0818L32 16.9354L22.314 14.3489L31.1444 11.9908L30.2984 8.84437L20.6128 11.4308L27.0768 4.98873L24.7656 2.68538L17.7737 9.65357L20.1424 0.843087Z" fill="#367AFF"/>
 <path d="M22.3776 17.7771C22.1069 18.9176 21.5354 19.9421 20.7513 20.763L27.1033 27.0935L29.4145 24.7901L22.3776 17.7771Z" fill="#367AFF"/>
@@ -88,6 +96,7 @@ const page = () => {
           <input
             type="text"
             id="name"
+            required
             value={name}
             onChange={handleNameChange}
             placeholder=" "
@@ -108,6 +117,7 @@ const page = () => {
             type="date"
             id="dob"
             value={dob}
+            required
             onChange={handleDobChange}
             className="peer w-full rounded-md border border-gray-400 px-3 pt-5 pb-2 text-base text-gray-900 
                        focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none
@@ -130,6 +140,7 @@ const page = () => {
     id="email"
     onChange={handleEmailChange}
     value={email}
+    required
     placeholder=' '
     className="peer w-full rounded-md border border-gray-400 px-3 pt-5 pb-2 text-base text-gray-900 
                focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none"
@@ -155,6 +166,7 @@ const page = () => {
     placeholder=" "
     onChange={handleOTPChange}
     value={otp}
+    required
     className="peer w-full rounded-md border border-blue-500 px-3 pt-5 pb-2 text-base text-gray-900 
                focus:border-blue-500 focus:ring-1 focus:ring-blue-500 focus:outline-none"
   />
