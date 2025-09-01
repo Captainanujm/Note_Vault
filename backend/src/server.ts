ca5961fa27cd3ec8f7d87da800efcfaafa4e3841
Anuj mishra <captainanuj2004@gmail.com>
2025-09-02 01:43:05 +0530

    cors issue

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,10 @@ import User from "./models/User";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "https://note-vault-kappa.vercel.app"],
+  credentials: true,
+}));
 const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON
